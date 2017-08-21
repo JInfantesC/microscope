97c9fa6b33641e957e69c86abb8962e0ee3763d6
JInfantesC <jose.infantes@protonmail.com>
2017-08-21 21:26:13 +0200

    Added post item login

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,18 @@
+Template.postItem.helpers({
+/*JavaScript Magic
+Although this is not specific to Meteor, here's a quick explanation of the above
+bit of “JavaScript magic”. First, we're creating an empty anchor (a) HTML
+element and storing it in memory.
+
+We then set its href attribute to be equal to the current post's URL (as we've
+just seen, in a helper this is the object currently being acted upon).
+
+Finally, we take advantage of that a element's special hostname property to get
+back the link's domain name without the rest of the URL.
+*/
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
+});
