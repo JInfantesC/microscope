eb89c35953384bbe3e4dede216a564cdb2767e91
JInfantesC <jose.infantes@protonmail.com>
2017-08-22 23:28:50 +0200

    Set connection to database in postList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,6 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+    posts: function() {
+        //Posts is accesible by whole app becouse was declared without var in collections/posts.js
+        return Posts.find();
+    }
 });
