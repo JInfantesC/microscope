9d57a51754e1aad5f3dd6974b99e78d2e702edaf
JInfantesC <jose.infantes@protonmail.com>
2017-08-27 11:49:49 +0200

    Added sorting

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,6 +1,10 @@
 Template.postsList.helpers({
     posts: function() {
         //Posts is accesible by whole app becouse was declared without var in collections/posts.js
-        return Posts.find();
+        return Posts.find({}, {
+            sort: { //Ordered with submit date descending order (-1)
+                submitted: -1
+            }
+        });
     }
 });
