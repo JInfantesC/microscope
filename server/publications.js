94408218074f47302392a48562b83257eb087b21
JInfantesC <jose.infantes@protonmail.com>
2017-09-11 20:17:52 +0200

    Updated comments publication/subcription

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,9 @@
 Meteor.publish("posts", function(argument) {
     return Posts.find();
 });
-Meteor.publish('comments', function() {
-    return Comments.find();
+Meteor.publish('comments', function(postId) {
+    check(postId, String);
+    return Comments.find({
+        postId: postId
+    });
 });
