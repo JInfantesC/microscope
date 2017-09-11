21da144efe5835685cb8647e5b74c78764b4ef65
JInfantesC <jose.infantes@protonmail.com>
2017-09-11 20:32:16 +0200

    Denormalize number of comments

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -17,10 +17,5 @@ Template.postItem.helpers({
     },
     ownPost: function() {
         return this.userId === Meteor.userId();
-    },
-    commentsCount: function() {
-        return Comments.find({
-            postId: this._id
-        }).count();
     }
 });
