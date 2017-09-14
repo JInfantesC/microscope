30e0797134a07207f21658f9f7822e34f4fffc3f
JInfantesC <jose.infantes@protonmail.com>
2017-09-14 20:12:42 +0200

    Updated post and router to set limits

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,3 +1,7 @@
+/*
+And since we're setting the data context at the route level, we can now safely
+get rid of the posts template helper inside the posts_list.js file and just
+delete the contents of that file.
 Template.postsList.helpers({
     posts: function() {
         //Posts is accesible by whole app becouse was declared without var in collections/posts.js
@@ -8,3 +12,4 @@ Template.postsList.helpers({
         });
     }
 });
+*/
