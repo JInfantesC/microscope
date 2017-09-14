4ed825de43c7826f78d11f44c12e26c5ea1669c1
JInfantesC <jose.infantes@protonmail.com>
2017-09-14 20:01:25 +0200

    Display notifications

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,20 @@ Meteor.publish('comments', function(postId) {
         postId: postId
     });
 });
+/*
+We solve this issue with publications. We can use our publications to specify
+precisely which part of our collection we want to share with each browser.
+
+To accomplish this, we need to return a different cursor in our publication
+than Notifications.find(). Namely, we want to return a cursor that corresponds
+to the current user's notifications.
+
+Doing so is straightforward enough, as a publish function has the current
+user's _id available at this.userId:
+*/
 Meteor.publish('notifications', function() {
-    return Notifications.find();
+    return Notifications.find({
+        userId: this.userId,
+        read: false
+    });
 });
